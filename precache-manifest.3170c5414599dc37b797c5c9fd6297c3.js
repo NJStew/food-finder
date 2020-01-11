76cf701f8d582159bf3bba8330f2d0b2d15dcea3
Noah Stewart <noahjamesbusiness@gmail.com>
2020-01-10 23:53:24 -0600

    Updates

---
+++ precache-manifest.3170c5414599dc37b797c5c9fd6297c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4510142148fee4f48d719516277f6050",
+    "url": "/food-finder/index.html"
+  },
+  {
+    "revision": "6eb1d360c3f6521d447f",
+    "url": "/food-finder/static/css/main.28409d7f.chunk.css"
+  },
+  {
+    "revision": "68496447b064c4106251",
+    "url": "/food-finder/static/js/2.5b1273c5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/food-finder/static/js/2.5b1273c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6eb1d360c3f6521d447f",
+    "url": "/food-finder/static/js/main.2bcbc131.chunk.js"
+  },
+  {
+    "revision": "c45fe8f2f5326651aa05",
+    "url": "/food-finder/static/js/runtime-main.10ac3c60.js"
+  },
+  {
+    "revision": "bda26c6e98f3492bac978d366e2fc413",
+    "url": "/food-finder/static/media/backbtn.bda26c6e.png"
+  },
+  {
+    "revision": "5bd703a45372b86fc1fa38dcdee55037",
+    "url": "/food-finder/static/media/finderbtn.5bd703a4.png"
+  }
+]);
